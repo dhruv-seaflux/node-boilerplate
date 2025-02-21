1ca4098075e5d9ba53edf38189eb2f6d8b1a84e0
dhruv kapadi <dhruv.kapadi@seaflux.tech>
2025-02-21 12:37:43 +0530

    remove deprecated configuration files and middleware

--- src/common/utils/get-repo.ts
+++
@@ -1,7 +0,0 @@
-import type { EntityTarget, Repository, ObjectLiteral } from "typeorm";
-import { getDB } from "../../config/db";
-
-export const getRepo = <T extends ObjectLiteral>(target: EntityTarget<T>): Repository<T> => {
-    return getDB().getRepository(target);
-  };
-  
